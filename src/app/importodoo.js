899aca9a9efb21b1c571abf4e825c4171cdb9901
dsportes <daniel.sportes@laposte.net>
2020-05-16 16:29:20 +0200

    Pour la prod: https selection

--- src/app/importodoo.js
+++ src/app/importodoo.js
@@ -24,8 +24,7 @@ const Odoo = remote.require('odoo')
 // import { Odoo } from './odoo'
 import { config } from './config'
 
-const minCB = config.minCB
-const maxCB = config.maxCB
+
 const map = config.map || { 'id': 'id', 'image': 'image', 'name': 'nom', 'barcode': 'code-barre', 'list_price': 'prix', 'categ_id': 'categorie', 'uom_id': 'unite' }
 
 /*
@@ -35,7 +34,9 @@ Categories d'articles acceptées. Remplacées par celle indiquée dans la map. S
 */
 const categories = config.categories || { 'défaut': 'A' }
 function transcodeCategorie (c) {
-    return categories[c] || categories['défaut']
+    let i = c.lastIndexOf('/ ')
+    let x = i === -1 ? c : c.substring(i + 2)
+    return categories[x] || categories['défaut']
 }
 
 /* Liste des propriétés de product.product à récupérer */
@@ -43,15 +44,17 @@ const fields = []
 for (let f in map) { fields.push(f) }
 
 /* Curieux nom : c'est la condition de filtre des produits pour l'API */
-const domain = [['barcode', '>=', minCB], ['barcode', '<=', maxCB], ['sale_ok', '=', true]]
+// const domain = [['barcode', '>=', minCB], ['barcode', '<=', maxCB], ['sale_ok', '=', true]]
+const domain = config.selection
 
 const odoo = new Odoo({
+    https: config.https || false,
     host: config.host,
     port: config.port,
     database: config.database,
     username: config.username,
     password: config.password,
-    timeout: 5000
+    timeout: config.timeoutodoo || 5000
 })
 
 function codeDeId(x) {
@@ -79,19 +82,21 @@ export function getArticles () {
                     } else {
                         const res = []
                         for (let i = 0, r = null; (r = products[i]); i++) {
+                            // console.log(JSON.stringify(r))
                             const a = {}
                             // mapping entre les champs reçus et les noms des colonnes (propriété de l'article)
-                            for (let f in map) { a[map[f]] = '' + r[f] }
+                            for (let f in map) { if (r[f]) a[map[f]] = '' + r[f] }
                             /*
                             Les champs uom_id (unite) et categ_id (categorie) sont à traiter : le code figure après la virgule
                             */
-                           a.unite = codeDeId(a.unite)
-                           let c = transcodeCategorie(codeDeId(a.categorie))
-                           if (c) {
-                                a.categorie = c
-                                res.push(a)
-                            }
-                        }
+                            a.unite = codeDeId(a.unite)
+                            let c = transcodeCategorie(a.categorie)
+                            if (c) {
+                                 a.categorie = c
+                                 // console.log(JSON.stringify(a))
+                                 res.push(a)
+                             }
+                         }
                         resolve(res)
                     }
                 })
